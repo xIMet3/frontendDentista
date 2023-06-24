c0a358a6542ae20a6395bf510adf9cc2e37635b4
xIMet3 <joaquin19_91@hotmail.com>
2023-06-24 21:48:11 +0200

    [update]Organise the navbar and put each component in its place. // Organizo la navbar y coloco cada componente en su sitio.

--- src/Common/Header/Header.jsx
+++ src/Common/Header/Header.jsx
@@ -1,34 +1,25 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
 import { BotonCambiaVista } from "../../Common/BotonCambiaVista/BotonCambiaVista";
 import { BotonCambiaVista2 } from "../../Common/BotonCambiaVista2/BotonCambiaVista2";
 
-
 export const Header = () => {
-    return(
-        <div className="headerGeneral">
-
-            <div className="botonIconoPrincipal">
-                <BotonCambiaVista2
-                path={'/Home'}
-                name={'GeeksDent'}
-                />
-            </div>
-
-            <div className="botonUno">
-                <BotonCambiaVista
-                path={'/Login'}
-                name={'Login'}
-                />
-            </div>
-            <div className="botonDos">
-                <BotonCambiaVista
-                path={'/Register'}
-                name={'Registro'}
-            />     
-            </div>
-            
+  return (
+    <div className="headerGeneral">
+      <div className="headerIzquierda">
+        <div className="botonIconoPrincipal">
+          <BotonCambiaVista2 path={"/Home"} name={"GeeksDent"} />
         </div>
+      </div>
 
-    )
-}
+      <div className="headerDerecha">
+        <div className="botonUno">
+          <BotonCambiaVista path={"/Login"} name={"Login"} />
+        </div>
+        <div className="botonDos">
+          <BotonCambiaVista path={"/Register"} name={"Registro"} />
+        </div>
+      </div>
+    </div>
+  );
+};
