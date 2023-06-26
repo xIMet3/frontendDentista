92669e30b89151c98df1434c04313bd3976350e6
xIMet3 <joaquin19_91@hotmail.com>
2023-06-26 20:46:50 +0200

    [feat]Add the hamburger menu to the navbar so that in small screens the login and register are saved in it. // Añado el menu hamburguesa a la navbar para que en pantallas pequeñas se guarden en ella el login y el register.

--- src/Common/Header/Header.jsx
+++ src/Common/Header/Header.jsx
@@ -6,56 +6,26 @@ import Navbar from 'react-bootstrap/Navbar';
 import { BotonCambiaVista } from "../../Common/BotonCambiaVista/BotonCambiaVista";
 import { BotonCambiaVista2 } from "../BotonCambiaVista2/BotonCambiaVista2";
 
-
 export const Header = () => {
   return (
     <div className="navbarComun">
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand >
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg" collapseOnSelect>
+        <Container >
+          <Navbar.Brand>
             <BotonCambiaVista2 path={"/Home"} name={"GEEKSDENT"} />
           </Navbar.Brand>
-          <Nav className="navbar-links">
-          <BotonCambiaVista path={"/Login"} name={"LOGIN"} />
-          <BotonCambiaVista path={"/Register"} name={"REGÍSTRATE"} />  
-          </Nav>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="navbar-links">
+              <BotonCambiaVista path={"/Login"} name={"LOGIN"} />
+              <BotonCambiaVista path={"/Register"} name={"REGÍSTRATE"} />
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
   );
 }
-
-  
-
-
-
-
-
-
-
-
-  // <div className="headerGeneral">
-  //   <div className="headerIzquierda">
-  //     <div className="botonIconoPrincipal">
-  //       <BotonCambiaVista2 path={"/Home"} name={"GeeksDent"} />
-  //     </div>
-  //   </div>
-
-  //   <div className="headerDerecha">
-  //     <div className="botonUno">
-  //       <BotonCambiaVista path={"/Login"} name={"Login"} />
-  //     </div>
-  //     <div className="botonDos">
-  //       <BotonCambiaVista path={"/Register"} name={"Registro"} />
-  //     </div>
-  //   </div>
-  // </div>
-
-
-
-
-
-
 
 
 
