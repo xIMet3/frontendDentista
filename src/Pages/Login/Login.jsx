0b3ca25a0df79b2922ab9e510bba85aa056a9d22
xIMet3 <joaquin19_91@hotmail.com>
2023-06-29 18:51:17 +0200

    [feat]Debugging the Login code by removing the elements that did nothing. Added comments and formatting. // Depuracion del codigo del Login quitando los elementos que no hacian nada. Se añaden comentarios y se da formato.

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,98 +1,99 @@
 import React from "react";
 import "./Login.css";
 import { useState } from "react";
-import {InputText} from '../../Common/InputText/InputText'
-import { loginUser } from "../../Services/ApiCalls"
+import { loginUser } from "../../Services/ApiCalls";
 import { login } from "../userSlice";
 import jwtDecode from "jwt-decode";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
-
-
-
+import Button from "react-bootstrap/Button";
 
 export const Login = () => {
+  // Define el estado "user" inicializado con las propiedades email y password vacias
   const [user, setUser] = useState({
     email: "",
     password: "",
-    });
+  });
+  // Obtiene la funcion dispatch de react-redux para despachar acciones
+  const dispatch = useDispatch();
+  // Obtiene la funcion navigate de react-router-dom para la navegacion
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const inputHandler = ({ target }) => {
+    let { name, value } = target;
+    setUser((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }; // Maneja los cambios en los campos de entrada y actualiza el estado "user"
 
-    const inputHandler = ({ target }) => {
-      let { name, value } = target;
-      setUser((prevState) => ({
-          ...prevState,
-          [name] : value
-      }))
-      };
-
-      const submitHandler = (e, body) => {
-        e.preventDefault()
-        loginUser(body)
-        .then(res => {
-            let decoded = jwtDecode(res)
-            dispatch(
-                login({
-                    token: res,
-                    name: decoded.name,
-                    role: decoded.rol
-                })
-            )
-            navigate("/")
+  const submitHandler = (e, body) => {
+    e.preventDefault(); // Evita el comportamiento predeterminado del evento
+    loginUser(body).then((res) => {
+      // Decodifica el token
+      let decoded = jwtDecode(res);
+      // Despacha la accion login con los datos del usuario
+      dispatch(
+        login({
+          token: res,
+          name: decoded.name,
+          role: decoded.rol,
         })
-        }
+      );
+      // Redirige al usuario a la página principal
+      navigate("/");
+    });
+  }; // Maneja el envío del formulario y realiza la llamada a la API para iniciar sesión
 
-        return (
-          <>
-              <Container>
-              <Row className="justify-content-center m-5">
-                  <Col
-                  xs={10}
-                  md={6}
-                  >
-                  <Form>
-                      <Form.Group
-                      className="mb-3"
-                      controlId="formBasicEmail"
-                      >
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                          type="email"
-                          name="email"
-                          placeholder="Enter email"
-                          onChange={(e)=>{inputHandler(e)}}
-                      />
-                      </Form.Group>
-      
-                      <Form.Group
-                      className="mb-3"
-                      controlId="formBasicPassword"
-                      >
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control
-                          type="password"
-                          name="password"
-                          placeholder="Password"
-                          onChange={(e)=>{inputHandler(e)}}
-                      />
-                      </Form.Group>
-                      <Button
-                      variant="primary"
-                      type="submit"
-                      onClick={(e)=>{submitHandler(e, user)}}
-                      >
-                      Submit
-                      </Button>
-                  </Form>
-                  </Col>
-              </Row>
-              </Container>
-          </>
-          );
-      };
+  return (
+    <>
+      <Container>
+        <Row className="formularioLogin justify-content-center m-5">
+          <Col xs={10} md={6}>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                  onChange={(e) => {
+                    inputHandler(e);
+                  }}
+                />
+              </Form.Group>
+              {/* Se le asigna la funcion inputHandler al evento onChange para manejar los cambios
+              en el campo de entrada. */}
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={(e) => {
+                    inputHandler(e);
+                  }}
+                />
+                {/* Se crea un boton de tipo de "submit" para enviar el formulario. Se le asigna la funcion 
+              submitHandler al evento onClick, pasando el evento y el objeto 'user' como argumentos.*/}
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={(e) => {
+                  submitHandler(e, user);
+                }}
+              >
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
 export default Login;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import './Register.css';
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
+// import { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Col from 'react-bootstrap/Col';
+// import Form from 'react-bootstrap/Form';
+// import InputGroup from 'react-bootstrap/InputGroup';
+// import Row from 'react-bootstrap/Row';
 
 
 
