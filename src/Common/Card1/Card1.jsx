6bb6e3c46c15f7b31024ad306adc1c93acf922e2
xIMet3 <joaquin19_91@hotmail.com>
2023-07-01 01:38:09 +0200

    [update]Modification of the home cards so that they are displayed correctly on small screens. // Modificacion de las cards del home para que se muestren correctamente en pantallas pequeñas.

--- src/Common/Card1/Card1.jsx
+++ src/Common/Card1/Card1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import "./Card1.css"
 
 const CardComponent = ({ title, content, price }) => {
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,83 +5,70 @@ import CardComponent from "../../Common/Card1/Card1";
 
 export const Home = () => {
   return (
-      <div className="paginaEntera">
-        <Container className="zonaCards">
-          <Row>
-            <Col xs={12} md={4}>
-              <Row>
-                <div className="primeraCard">
-                  <CardComponent
-                    title="REVISIÓN BUCAL"
-                    content="Revisión de dientes y encias."
-                    price="24.95€"
-                  />
-                </div>
-              </Row>
-              <Row>
-                <div className="segundaCard">
-                  <CardComponent
-                    title="BLANQUEAMIENTO BUCAL"
-                    content="Blanqueamiento dental."
-                    price="49.95€"
-                  />
-                </div>
-              </Row>
-              <Row>
+    <div className="paginaEntera">
+      <Container className="zonaCards">
+        <Row>
+          <Col xs={12} md={4}>
+            <Row>
+              <div className="primeraCard">
+                <CardComponent
+                  title="REVISIÓN BUCAL"
+                  content="Revisión de dientes y encias."
+                  price="24.95€"
+                />
+              </div>
+            </Row>
+            <Row>
+              <div className="segundaCard">
+                <CardComponent
+                  title="BLANQUEAMIENTO BUCAL"
+                  content="Blanqueamiento dental."
+                  price="49.95€"
+                />
+              </div>
+            </Row>
+            <Row>
               <div className="sextaCard">
-                  <CardComponent
-                    title="ORTODÓNCIA"
-                    content="Estudio y colocación de ortodóncia dental y posterior seguimiento."
-                    price="Desde 2499.95€"
-                  />
-                </div>
-              </Row>
-            </Col>
-            <Col xs={12} md={4}>
-            </Col>
-            <Col xs={12} md={4}>
-              <Row>
-                <div className="cuartaCard">
-                  <CardComponent
-                    title="LIMPIEZA BUCAL"
-                    content="Revisión y limpieza a fondo de dientes y encias"
-                    price="39.95€"
-                  />
-                </div>
-              </Row>
-              <Row>
-                <div className="quintaCard">
-                  <CardComponent
-                    title="IMPLANTE"
-                    content="Extracción y colocación de implante."
-                    price="899.95€"
-                  />
-                </div>
-              </Row>
-              <Row>
+                <CardComponent
+                  title="ORTODÓNCIA"
+                  content="Estudio y colocación de ortodóncia dental y posterior seguimiento."
+                  price="Desde 2499.95€"
+                />
+              </div>
+            </Row>
+          </Col>
+          <Col xs={12} md={4}></Col>
+          <Col xs={12} md={4}>
+            <Row>
+              <div className="cuartaCard">
+                <CardComponent
+                  title="LIMPIEZA BUCAL"
+                  content="Revisión y limpieza a fondo de dientes y encias"
+                  price="39.95€"
+                />
+              </div>
+            </Row>
+            <Row>
+              <div className="quintaCard">
+                <CardComponent
+                  title="IMPLANTE"
+                  content="Extracción y colocación de implante."
+                  price="899.95€"
+                />
+              </div>
+            </Row>
+            <Row>
               <div className="terceraCard">
-                  <CardComponent
-                    title="EXTRACCIÓN"
-                    content="Extracción de pieza dental."
-                    price="49.95€"
-                  />
-                </div>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+                <CardComponent
+                  title="EXTRACCIÓN"
+                  content="Extracción de pieza dental."
+                  price="49.95€"
+                />
+              </div>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
