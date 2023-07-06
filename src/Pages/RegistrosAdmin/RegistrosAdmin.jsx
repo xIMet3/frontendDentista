e37115ca75858c6736d475888dc5ab7d3427ce45
xIMet3 <joaquin19_91@hotmail.com>
2023-07-06 19:42:04 +0200

    [feat]Added design to the RegistrosAdmin view so that each user is shown in a different card. Also added the background. // Añadido diseño a la vista RegistrosAdmin para que cada usuario se muestre en una card distinta. Tambien añadido el fondo.

--- src/Pages/RegistrosAdmin/RegistrosAdmin.jsx
+++ src/Pages/RegistrosAdmin/RegistrosAdmin.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
-import "./RegistrosAdmin.css"
+import "./RegistrosAdmin.css";
 import { getAllProfiles } from "../../Services/ApiCalls";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { userData } from "../userSlice";
 
 const AllUsers = () => {
   const [users, setUsers] = useState([]);
-  const credentialsRedux  = useSelector(userData);
+  const credentialsRedux = useSelector(userData);
   const token = credentialsRedux?.credentials?.token;
-  console.log(users)
+  console.log(users);
 
   useEffect(() => {
     getAllProfiles(token).then((res) => {
@@ -17,27 +17,24 @@ const AllUsers = () => {
   }, []);
 
   return (
-    <div className="cardsUsers">
-      <h1>Todos los usuarios registrados</h1>
-      {
-        
-      users.length === 0 
-      
-      ? (
-        <p>No hay usuarios registrados</p>
-      ) 
-      
-      : (
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>
-              Nombre: {user.name}, 
-              Email: {user.email}, 
-              Teléfono: {user.telephoneNumber}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="vistaCompleta">
+      <div className="cardsUsers">
+        <h1>Todos los usuarios registrados</h1>
+        {users.length === 0 ? (
+          <p>No hay usuarios registrados</p>
+        ) : (
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>
+                <div className="cardIndividual">
+                  Nombre: {user.name}, Email: {user.email}, Teléfono:{" "}
+                  {user.telephoneNumber}
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
